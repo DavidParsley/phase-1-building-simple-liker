c0103600ce7a3162768b21fc1166c07d30ae4e81
David Parsley <davidparsley.kakhayanga@gmail.com>
2024-10-10 22:43:43 +0300

    adds index.js

--- main.js
+++ main.js
@@ -3,9 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hide = document.querySelector("#modal")
+hide.classList.add("hidden")
 
+function likeClick(e){
+  e.addEventListener("click", ()=>{
+    const like = e.querySelector(".like-glyph")
+    if(like.textContent === EMPTY_HEART){
+      mimicServerCall()
+       .then((res)=>{
+        like.textContent = FULL_HEART
+        like.classList.add("activated-heart")
+        console.log(res);
+        
+       })
+       .catch((error)=>{
+        const errorMessage = document.getElementById("modal-message")
+        errorMessage.textContent = error
+        hide.classList.remove("hidden")
 
+        setTimeout(()=>{
+          hide.classList.add("hidden")
+        }, 3000)
+       })
 
+      } else{
+        like.textContent = EMPTY_HEART
+        like.classList.remove("activated-heart")
+        console.log(res);
+      }
+  })
+}
+
+const allButtons = document.querySelectorAll(".like")
+allButtons.forEach(button =>{
+  likeClick(button)
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
